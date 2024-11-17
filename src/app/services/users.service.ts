97bb290f123928d2d9351bc3200a4e65624aa9e9
rafa913 <rafaellacerda60@gmail.com>
2024-11-16 23:27:49 -0400

    criando regras do interface

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore} from '@angular/fire/compat/firestore'
 import { Observable } from 'rxjs';
+import { User } from '../interfaces/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
+
+  user: User = {
+  name:'Rafael Batista',
+  email:'rafael@mail.com',
+  sector:'Tecnologia',
+  role:'Desenvolvedor Front-End Sênior'
+
+  }
 
   constructor(private dataBaseStore: AngularFirestore) { }
 
@@ -13,12 +22,12 @@ export class UsersService {
     return this.dataBaseStore.collection('users', user => user.orderBy('name')).valueChanges({idField: 'firebaseId'}) as Observable<any[]>
   }
 
-  addUser(user: any) {
+  addUser(user: User) {
 
     return this.dataBaseStore.collection('users').add(user)
   }
 
-  upDate(userId: string, user:any){
+  upDate(userId: string, user:User){
     return this.dataBaseStore.collection('users').doc(userId).update(user)
   }
 
