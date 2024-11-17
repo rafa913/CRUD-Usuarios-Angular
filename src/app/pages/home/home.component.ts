7178451977932c82200991127f1518f7d878d30a
rafa913 <rafaellacerda60@gmail.com>
2024-11-16 22:10:07 -0400

    estilização home-component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  UserName:string | null;
+
+  ngOnInit() {
+    this.UserName = sessionStorage.getItem('User')
+  }
+
 }
