7a62c1fd8712d47e97c08f8c9136c3c18cd1d70f
rafa913 <rafaellacerda60@gmail.com>
2024-11-22 12:53:34 -0400

    logica botao +casdastrar

--- src/app/pages/crud/modal-form-user/modal-form-user.component.ts
+++ src/app/pages/crud/modal-form-user/modal-form-user.component.ts
@@ -1,70 +1,85 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
+import { UsersService } from '../../../services/users.service';
+import { User } from '../../../interfaces/user';
+import { response } from 'express';
 
 @Component({
   selector: 'app-modal-form-user',
   templateUrl: './modal-form-user.component.html',
-  styleUrl: './modal-form-user.component.scss'
+  styleUrl: './modal-form-user.component.scss',
 })
 export class ModalFormUserComponent {
-
-planosSaude = [
-  {
-  id: 1,
-  descricao:'Plano 300 Enfermaria'
-  },
-  {
-  id: 2,
-  descricao:'Plano 400 Enfermaria'
-  },
-
-  {
-  id: 3,
-  descricao:'Plano 500 Plus'
-  }
-]
-
- planosOdonto = [
-  {
-    id: 1,
-    descricao:'Plano Basic'
+  planosSaude = [
+    {
+      id: 1,
+      descricao: 'Plano 300 Enfermaria',
     },
-  {
-    id: 2,
-    descricao:'Plano Medium'
-    },
-  {
-    id: 3,
-    descricao:'Plano Plus'
+    {
+      id: 2,
+      descricao: 'Plano 400 Enfermaria',
     },
 
- ]
+    {
+      id: 3,
+      descricao: 'Plano 500 Plus',
+    },
+  ];
+
+  planosOdonto = [
+    {
+      id: 1,
+      descricao: 'Plano Basic',
+    },
+    {
+      id: 2,
+      descricao: 'Plano Medium',
+    },
+    {
+      id: 3,
+      descricao: 'Plano Plus',
+    },
+  ];
 
   formUser: FormGroup;
 
   constructor(
-    public  diaLogRef: MatDialogRef<ModalFormUserComponent>,
-    private formBuilder: FormBuilder
-  ){}
+    public diaLogRef: MatDialogRef<ModalFormUserComponent>,
+    private formBuilder: FormBuilder,
+    private userService: UsersService
+  ) {}
 
-  ngOnInit(){
-
+  ngOnInit() {
     this.buildForm();
   }
 
-  buildForm(){
-    this.formUser = this.formBuilder.group({
-      name:[null, [Validators.required, Validators.minLength(3)]],
-      email:[null, [Validators.required, Validators.email]],
-      sector:[null, [Validators.required, Validators.minLength(2)]],
-      role:[null, [Validators.required, Validators.minLength(5)]],
-      healthPlan:[''],
-      dentalPlan:[''],
-      });
+  saveUser() {
+    const objUserForm: User = this.formUser.getRawValue();
 
+    this.userService.addUser(objUserForm).then((response: any) => {
+      window.alert('Usuário Salvo com Sucesso');
+      this.closeModal();
+    })
+    .catch(
+      err => {
+        window.alert('Ocorreu um erro ao salvar o usuário')
+        console.error(err)
+      })
   }
- 
-  closeModal(){this.diaLogRef.close();}
 
+  buildForm() {
+    this.formUser = this.formBuilder.group({
+      name: [null, [Validators.required, Validators.minLength(3)]],
+      email: [null, [Validators.required, Validators.email]],
+      sector: [null, [Validators.required, Validators.minLength(2)]],
+      role: [null, [Validators.required, Validators.minLength(5)]],
+      healthPlan: [''],
+      dentalPlan: [''],
+    });
+  }
+
+  closeModal() {
+    this.diaLogRef.close();
+  }
 }
